09a28183109dc2ec490a90c10dc65f0ed8ed640c
Ketere <Keterel8@gmail.com>
2023-10-17 07:13:52 +0000

    deleted files

--- cray_print.c
+++
@@ -1,5 +0,0 @@
-#include "cray_shell.h"
-
-void cray_print(const char *crayprint) {
-    write(STDOUT_FILENO, crayprint, strlen(crayprint));
-}
--- cray_prompt.c
+++
@@ -1,4 +0,0 @@
-#include "cray_shell.h"
-void cray_prompt(void) {
-    cray_print("cray_shell$$ ");
-}
